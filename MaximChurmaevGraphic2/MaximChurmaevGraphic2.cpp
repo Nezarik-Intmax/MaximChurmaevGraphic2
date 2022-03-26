69aba902afbb299686b741c8a8af55ae1e6d0f7c
User <User@STD08>
2022-03-26 17:33:33 +0500

    Добавьте файлы проекта.

---
+++ MaximChurmaevGraphic2/MaximChurmaevGraphic2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "GL/glew.h";
+#include "GL/glut.h";
+#include "glm/glm.hpp";
+//include "GL/freeglut.h";
+
+void RenderSceneOB() {
+	glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");
+	glutDisplayFunc(RenderSceneOB);
+	glutMainLoop();
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s' \n", glewGetErrorString(res));
+		return 1;
+	}
+
+	
+
+}
